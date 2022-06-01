c69893feedf5f9f3d4c199dba2439e3997943ee6
captjay98 <captjay98@gmail.com>
2022-06-01 05:53:34 +0100

    Easy Peasy

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,10 +2,10 @@
 
 def fizzbuzz():
 
-    for i in range(101):
+    for i in range(1, 101):
         if i % 3 == 0:
             print("Fizz ", end='')
-        elif i % 5 == 0:
+        elif i % 3 and i % 5 == 0:
             print("FizzBuzz ", end='')
         else:
             print(f"{i} ", end='')
