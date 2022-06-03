e8273af248e597faecfc3f6b1fc700f2c14108d1
captjay98 <captjay98@gmail.com>
2022-06-03 04:01:38 +0100

    Accompanying file for tasks

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def add(a,b):
+    x = a+b
+    return x
---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    x = a + b
+    return x
+
+def sub(a, b):
+    x = a - b
+    return x
+
+def mul(a, b):
+    x = a * b
+    return x
+
+def div(a, b):
+    x = a // b
+    return x
+
