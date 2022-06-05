eba9c58cb848a60bccfec75d3a81824649ba0eed
captjay98 <captjay98@gmail.com>
2022-06-05 02:46:33 +0100

    Task 7 Done

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
+    for row in range(len(matrix)):
+        for column in range(len(matrix[row])):
+            print("{:d}".format(matrix[row][column]), end=" ")
 
-    for item in matrix:
-        print("{}".format(item))
+        print("")
