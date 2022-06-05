5548e862c416be35869108494b58b80bd982802c
captjay98 <captjay98@gmail.com>
2022-06-05 03:35:29 +0100

    Task 10 Done

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -9,8 +9,3 @@ def max_integer(my_list=[]):
             largest = item
 
     return largest
-
-
-my_list = [1, 90, 2, 13, 34, 5, -13]
-max_value = max_integer(my_list)
-print("Max: {}".format(max_value))
