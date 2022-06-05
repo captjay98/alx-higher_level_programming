47c1aa00e062ceca7f4d7cc92b655035e0bb65ec
captjay98 <captjay98@gmail.com>
2022-06-05 01:31:50 +0100

    Task 5 done

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -6,6 +6,7 @@ def new_in_list(my_list, idx, element):
     elif idx > len(my_list) - 1:
         return my_list
     else:
-        new_list = my_list.remove(idx + 1)
-        new_list = my_list.insert(idx, element)
-        return new_list
+        new = my_list.copy()
+        new = my_list.remove(idx + 1)
+        new = my_list.insert(idx, element)
+        return new
