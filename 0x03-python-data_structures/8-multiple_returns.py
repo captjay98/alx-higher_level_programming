f53e71589b0d5866b9a2cbf3387f447adfb3f4c4
captjay98 <captjay98@gmail.com>
2022-06-07 05:42:16 +0100

    Tak 9

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@
 def multiple_returns(sentence):
      if sentence == "":
         return (0, None)
-    char = len(sentence), sentence[0])
-    return char
+    
+     return len(sentence), sentence[0])
 
 
