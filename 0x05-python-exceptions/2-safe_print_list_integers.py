1d523f580cd8c48c3baaff5c37bc47062445286c
captjay98 <captjay98@gmail.com>
2022-06-21 01:11:32 +0100

    pycodestye

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,7 +2,7 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     num = 0
-        
+
     for item in range(x):
         try:
             print("{:d}".format(my_list[item]), end="")
@@ -12,16 +12,3 @@ def safe_print_list_integers(my_list=[], x=0):
     print("")
 
     return num
-
-
-my_list = [1, 2, 3, 4, 5]
-
-nb_print = safe_print_list_integers(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-
-my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
-nb_print = safe_print_list_integers(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-
-nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
