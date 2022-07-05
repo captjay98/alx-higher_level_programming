b17d1f64f348d566836eef1696a3e97ea845da5e
captjay98 <captjay98@gmail.com>
2022-07-05 05:21:57 +0100

    Task Four

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Defines another Class checking function """
+
+
+def is_kind_of_class(obj, a_class):
+    """function: is_kind_of_class
+    obj: an object
+    a_class: a class
+    Returns: Bool
+    """
+    return isinstance(obj, a_class)
