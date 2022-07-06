83a9fa6b860f15fc670798fee200243269c63398
captjay98 <captjay98@gmail.com>
2022-07-06 03:31:44 +0100

    Advanced Task One

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""a function that inserts a line of text to a file, after each line containing a specific string"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """a function that inserts a line of text to a file, after each line containing a specific string"""
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+    with open(filename, 'w') as f:
+        for line in lines:
+            f.write(line)
+            if search_string in line:
+                f.write(new_string)
+    return
