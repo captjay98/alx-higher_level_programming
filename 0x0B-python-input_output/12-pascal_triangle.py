bb9a60892e35ed9bc8ca5ad5005ade84f500d055
captjay98 <captjay98@gmail.com>
2022-07-06 03:28:33 +0100

    Task Twelve

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a Pascal Triangle Triangle"""
+
+
+def pascal_triangle(n):
+    """Returns a list of lists representing the Pascal's triangle of n rows"""
+    if n == 0:
+        return []
+    if n == 1:
+        return [[1]]
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
