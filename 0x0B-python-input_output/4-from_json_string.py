446288e17d067e6c7e24524f94db303f4729fd06
captjay98 <captjay98@gmail.com>
2022-07-06 02:55:56 +0100

    Task Four

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
-"""function that returns the JSON representation of an object (string)
+"""function that returns the JSON representation
+    of an object (string)
 """
-
 
 import json
 
 
 def from_json_string(my_str):
     """module from_json_strin
-    returns the object represented by a JSON string
+    returns python objects
     """
     return json.loads(my_str)
