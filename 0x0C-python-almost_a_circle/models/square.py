7772cd82d3fdb5e26d802ced56d91cb756cb764c
captjay98 <captjay98@gmail.com>
2022-07-13 03:17:46 +0100

    Square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,3 +13,11 @@ class Square(Rectangle):
         """Returns details on the square"""
         return ("[Square] (<{}>) <{}>/(<{}>) - <{}>"
                 .format(self.id, self.x, self.y, self.width))
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = self.height = value
