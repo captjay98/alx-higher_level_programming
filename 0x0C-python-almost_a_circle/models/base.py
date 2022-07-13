4601205eecfd3f0c4ecc23d76796a2466be250af
captjay98 <captjay98@gmail.com>
2022-07-13 03:24:26 +0100

    Added json dict

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Base Class for all other objects in this Project"""
+import json
 
 
 class Base:
@@ -10,9 +11,15 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """new """
+        """new  id"""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
