a1f9ad20ab100479d1ffe56754f64be53db2948a
captjay98WSL <captjay98@gmail.com>
2022-09-06 00:58:06 +0100

    Task Eleven fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,14 +2,17 @@
 
 const args = process.argv;
 
-if (!args[2]) {
-  console.log(0);
-} else if (args[2] === 1) {
+let largest = 0;
+const myarr = [];
+
+if (args[2] < 3) {
   console.log(0);
 } else {
-  for (let i = 0; i < args.length; i++) {
-    if (args[i] > args[i + 1]) {
-      console.log(args[i]);
+  for (let i = 2; i < args.length; i++) {
+    if (args[i] > largest) {
+      largest = args[i];
+      myarr.push(largest);
     }
   }
+  console.log(myarr[0]);
 }
