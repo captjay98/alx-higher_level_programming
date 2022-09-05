ab2dd05e1aac88eeaa21ccbbfa846b60404d03c5
captjay98WSL <captjay98@gmail.com>
2022-09-05 22:46:35 +0100

    task 3 fix

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,11 +2,8 @@
 
 const args = process.argv;
 
-if (args === 2) {
+if (args.length === 2) {
   console.log('No argument');
 } if (args[2]) {
   console.log(`${args[2]}`);
 }
-args.forEach((val, index) => {
-  console.log(`${index}: ${val}`);
-});
