459e4c4f50643f00289152d1ba6963623f4b906f
captjay98WSL <captjay98@gmail.com>
2022-09-05 23:38:31 +0100

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@
 const args = process.argv;
 
 if (isNaN(args[2]) === false) {
-  console.log('My number' + ' ' + args[2]);
+  console.log('My number:' + ' ' + args[2]);
 } else {
   console.log('Not a number');
 }
