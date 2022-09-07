8ad60ac753eb6342abafdc85ab8a9530ef300f0d
captjay98WSL <captjay98@gmail.com>
2022-09-07 01:59:04 +0100

    task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const Square_ = require('./5-square');
+
+const Square = class Square extends Square_ {
+  charPrint (c) {
+    if (c) {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+    } else {
+      this.print();
+    }
+  }
+};
+
+module.exports = Square;
