8c876701b57fdac589d99d458d7232efe85de994
captjay98WSL <captjay98@gmail.com>
2022-09-07 01:39:19 +0100

    task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,40 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const rec = 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(rec.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    let temp;
+    temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+const r1 = new Rectangle(2, 3);
+console.log('Normal:');
+r1.print();
+
+console.log('Double:');
+r1.double();
+r1.print();
+
+console.log('Rotate:');
+r1.rotate();
+r1.print();
