9a60041845000cd619a7f625c732d33cbd7aabcb
captjay98WSL <captjay98@gmail.com>
2022-09-07 01:41:09 +0100

    task 4 fix

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -26,15 +26,3 @@ module.exports = class Rectangle {
     this.height *= 2;
   }
 }
-
-const r1 = new Rectangle(2, 3);
-console.log('Normal:');
-r1.print();
-
-console.log('Double:');
-r1.double();
-r1.print();
-
-console.log('Rotate:');
-r1.rotate();
-r1.print();
