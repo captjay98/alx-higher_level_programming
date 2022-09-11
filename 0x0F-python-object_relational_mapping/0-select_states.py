0e798ee1e02087148e8e25e995574f4517e2eee2
captjay98WSL <captjay98@gmail.com>
2022-09-11 18:51:59 +0100

    TASK 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import MySQLdb
+from sys import argv
+
+user = argv[1]
+password = argv[2]
+database = argv[3]
+
+connection = MySQLdb.connect(
+    host='localhost',
+    port=3306,
+    user=user,
+    password=password,
+    database=database)
+
+cursor = connection.cursor()
+
+cursor.execute(" SELECT * FROM customers")
+
+db = cursor.fetchall()
+
+for item in db:
+    print(item)
