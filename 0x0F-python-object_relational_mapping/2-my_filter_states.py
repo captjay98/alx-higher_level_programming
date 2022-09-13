412ce569687f162434dd77959dfbb1df7b9238c0
captjay98WSL <captjay98@gmail.com>
2022-09-13 13:02:37 +0100

    Task two

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,14 +9,13 @@ from sys import argv
 if __name__ == "__main__":
     user, password, database, arg = argv[1], argv[2], argv[3], argv[4]
     db = MySQLdb.connect(
-                                            host='localhost', port=3306,
-                                            user=user, password=password,
-                                            database=database
-                                            )
+        host='localhost', port=3306,
+        user=user, password=password,
+        database=database
+    )
     cur = db.cursor()
     cur.execute(
         "SELECT * FROM states WHERE name LIKE '{:s}'"
-                        "ORDER BY id ASC".format(arg)
-            )
+        "ORDER BY id ASC".format(arg))
     for item in cur.fetchall():
         print(item)
