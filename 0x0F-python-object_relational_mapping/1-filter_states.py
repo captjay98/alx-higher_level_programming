091638d1b213c8e3d866cfd8d1ebbc5a916ee6bf
captjay98WSL <captjay98@gmail.com>
2022-09-13 12:59:35 +0100

    Task One

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-
-
-import MySQLdb
-from sys import argv
-
 """
 lists all states with starting name with N
 from the database hbtn_0e_0_usa
 """
+
+import MySQLdb
+from sys import argv
+
 if __name__ == "__main__":
     user, password, database = argv[1], argv[2], argv[3]
     db = MySQLdb.connect(
