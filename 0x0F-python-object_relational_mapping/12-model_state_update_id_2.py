44c64cf56c2894a938f85612587cf20413fd9a87
captjay98WSL <captjay98@gmail.com>
2022-09-13 22:39:05 +0100

    Task Twelve

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Write a script that changes the name 
+of a State object from the database hbtn_0e_6_usa
+"""
+
+from sys import argv
+from sqlalchemy .orm import declarative_base, sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+
+if __name__ == "__main__":
+
+    engine = create_engine(
+        f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}")
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    data = session.query(State).filter_by(id=2).first()
+    data.name = "New Mexico"
+    session.add(data)
+    session.commit()
+    session.close()
