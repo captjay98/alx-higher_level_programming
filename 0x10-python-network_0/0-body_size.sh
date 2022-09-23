68bf1c7f9b77564cdd85057ae1107b1f4d465a0c
captjay98WSL <captjay98@gmail.com>
2022-09-23 02:00:41 +0100

    done

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/bash
-# takes a request
-# sends a request to that URL displays the size of the response body
-curl -sI "$1" | grep -i Content-Length | cut -d " " -f2
+# takes a request sends a request to that URL displays the size of the response body
+curl -sI "$1" | grep "Content-Length" | cut -d " " -f2
